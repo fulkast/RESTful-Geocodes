3c4de395c5c2f1b308c5a339d60614eadd7cd186
Frank <fulkast@gmail.com>
2018-01-08 20:52:35 -0800

    adding geolocation tests

--- geocodes_tests.py
+++ geocodes_tests.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import string
 
 
 def testBasicConnection():
@@ -7,7 +8,27 @@ def testBasicConnection():
     results  = json.load(response)
     return True if results["status"] == "Connected" else False
 
+def testGeoCoding(input_string):
+    response = urllib2.urlopen("http://localhost:5000/geocodes/v1/"+input_string)
+    results = json.load(response)
+    if  results["status"] == "Result Found":
+        return results["location_name"] , True
+    else:
+        return input_string, False
+
 
 if __name__ == "__main__":
     print ("\nConnection Test Passed!" if testBasicConnection() else "\nConnection Test Failed!")
+    print
+
+    locations = ["Maputo", "Tokyo", "Rio de Janeiro", "Washington D.C.", "Malmo", "Qatar", "xyz"]
+    locations = [string.replace(location, " ", "+") for location in locations]
+
+    n_passed_tests = 0
+    for location in locations:
+        location_name, success = testGeoCoding(location)
+        print (location_name + (" was successfully found "  if success else " was not found"))
+        if success: n_passed_tests += 1
+
+    print(str(n_passed_tests) + " tests passed\n" + str(len(locations) - n_passed_tests) + " tests failed")
     print
